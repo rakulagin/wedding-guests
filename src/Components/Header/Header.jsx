13afe80d3b62bef189923f4e5fdb775bdca853c8
rakulagin <koulagin@gmail.com>
2023-06-25 10:58:36 +0400

    2 commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="p-2 bg-primary ">
+      <div className="container h4 text-white ">Наши гости</div>
+    </div>
+  );
+};
+
+export default Header;
