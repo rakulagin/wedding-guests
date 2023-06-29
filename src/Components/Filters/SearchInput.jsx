650d4e6bbe45ce44fbd3eaaced87e3de5f91decf
Roman <koulagin@gmail.com>
2023-06-29 10:59:08 +0400

    фильтр

---
+++ src/Components/Filters/SearchInput.jsx
@@ -0,0 +1,8 @@
+import React, {useState} from "react";
+
+const SearchInput = ({searchValue, setSearchValue}) => {
+    const [searchValue, setSearchValue] = useState('')
+
+}
+
+export default SearchInput
